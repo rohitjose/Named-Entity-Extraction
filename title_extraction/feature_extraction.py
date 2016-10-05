b8439c7fa520c5fbc21c3ab94f3eba872d1da5a3
Rohit <rohitjose@gmail.com>
2016-10-06 02:22:50 +1100

    Implemented feature extraction for binary valued features

--- title_extraction/feature_extraction.py
+++ title_extraction/feature_extraction.py
@@ -1,39 +1,74 @@
 import pickle
 import sys
-import nltk
 import pandas
+from sklearn.linear_model import LogisticRegression
+
+
+def check_word_capitalization(word):
+    """Checks whether a word is a capitalized word"""
+    return_value = False
+    if (len(word) > 1):
+        return_value = True if (word[0].isupper() and word[1].islower()) else False
+    return return_value
+
 
 def build_training_features(training_data):
     # Load the data set
     with open(str(training_data), 'rb') as f:
         training_set = pickle.load(f)
 
-    print(training_set[0])
-    columns = ["isUpper","isCapitalized"]
+    columns_values = ["word","POStag","TitleMapping","isUpper", "isCapitalized", "isNNP", "isNN", "isJJ","isPrecedingDT","isPrecedingIN","isTitle"]
+    preceding_pos_tag = None
     for sentence in training_set:
         for word in sentence:
-            # Check for Uppercased word
-            if (word[0].isupper()):
-                word.append(1)
-            else:
-                word.append(0)
+            # WORD TEXT RELATED FEATURES
+            # Check for a word in uppercase
+            word.append(1 if (word[0].isupper()) else 0)
+            # Check for word capitalization
+            word.append(1 if (check_word_capitalization(word[0])) else 0)
 
-            # Check for capitalization
-            if(word[0][0].isupper()):
-                word.append(1)
-            else:
-                word.append(0)
+            # WORD POS TAGGING RELATED FEATURES
+            # Check if the POS tagging is NNP - (noun, proper, singular)
+            word.append(1 if (word[1] == 'NNP') else 0)
+            # Check if the POS tagging is NN - (noun, common, singular or mass)
+            word.append(1 if (word[1] == 'NN') else 0)
+            # Check if the POS tagging is JJ - (adjective or numeral, ordinal)
+            word.append(1 if (word[1] == 'JJ') else 0)
 
-    frame = [item for sublist in training_set for item in sublist]
-    for word in frame:
-        if(word[-1]==1):
-            print(word)
+            # POS TAGGING OF PRECEDING WORD
+            # Check if the preceding POS tagging was DT
+            word.append(1 if (preceding_pos_tag == 'DT') else 0)
+            # Check if the preceding POS tagging was IN (preposition or conjunction, subordinating)
+            word.append(1 if (preceding_pos_tag == 'IN') else 0)
 
-if __name__=="__main__":
+            # TARGET COLUMN
+            word.append(1 if(word[2]=='TITLE') else 0)
+            preceding_pos_tag = word[1]
+
+    # Flat list of all the words
+    word_list = [item for sublist in training_set for item in sublist]
+    # Pandas Data frame
+    word_frame = pandas.DataFrame(word_list,columns=columns_values)
+    # Remove the first 3 columns [word, POS tag, target]
+    data_frame = word_frame[columns_values[3:]]
+
+    return data_frame
+
+def trainRegressionModel(X,y):
+    """Trains the logistic regression model based on the extracted feature values given"""
+    # instantiate a logistic regression model, and fit with X and y
+    model = LogisticRegression()
+    model = model.fit(X, y)
+    # check the accuracy on the training set
+    print(model.score(X, y))
+
+
+if __name__ == "__main__":
     # Get the training data set path
     training_data = sys.argv[1]
-
-    build_training_features(training_data)
-
-
+    columns_values = ["isUpper", "isCapitalized", "isNNP", "isNN", "isJJ","isPrecedingDT", "isPrecedingIN", "isTitle"]
+    data = build_training_features(training_data)
+    X = data[columns_values[:-1]]
+    y = data["isTitle"]
+    trainRegressionModel(X,y)
 
--- title_extraction/training.py
+++ title_extraction/training.py
@@ -3,6 +3,9 @@ import sys
 import nltk
 import pandas
 
+# def calculate_NE_relative_index:
+#
+
 
 # Get the training data set path
 training_data = sys.argv[1]
@@ -11,40 +14,39 @@ training_data = sys.argv[1]
 with open(str(training_data),'rb') as f:
     training_set =  pickle.load(f)
 
-
-# # Parse the words list to build the
+columns = ['isCapitalized','isUpperCase','isPreceedingCharacterPreposition','isPreceedingCharacterDeterminer','isNN','isNNP','isJJ']
+#Parse the words list to build the features
+# count = 0
 # for pos in training_set:
 #     # sentence creation
 #     sentence = " ".join([word[0] for word in pos])
-#     print(sentence)
+#     #print(sentence)
 #     namedEnt = nltk.ne_chunk(pos, binary=False)
-#     print(namedEnt)
+#     #print(namedEnt)
+#     person = []
+#     for subtree in namedEnt.subtrees(filter=lambda t: t.label() in ['PERSON','ORGANIZATION','GPE','LOCATION','DATE','TIME','MONEY','PERCENT','FACILITY','GSP']):
+#         for leave in subtree.leaves():
+#             person.append(leave)
+#     if (person==[]):
+#         print(namedEnt)
+#         print(sentence)
+#         count += 1
+
+# pos = training_set[4]
+# sentence = " ".join([word[0] for word in pos])
+# person = []
+# namedEnt = nltk.ne_chunk(pos, binary=False)
+# for subtree in namedEnt.subtrees(filter=lambda t: t.label() in ['PERSON','ORGANIZATION','GPE']):
+#     for leave in subtree.leaves():
+#         person.append(leave)
+# print(person)
+# print(namedEnt)
+# for word in person:
+#     print(str(word)+str(pos.index(word)))
+
+#print(len(pos))
+
+print(len(training_set))
 
 
 frame = [item for sublist in training_set for item in sublist]
-
-count = {}
-
-for item in frame:
-    if(item[2]!='TITLE'):
-        if(item[1] in count):
-            count[item[1]] += 1
-        else:
-            count[item[1]] = 1
-        if(item[1]=='JJ'):
-            print(item)
-
-print(count)
-
-
-total_count = 0
-for d in count:
-    total_count+=count[d]
-
-print(total_count)
-
-
-
-
-
-
