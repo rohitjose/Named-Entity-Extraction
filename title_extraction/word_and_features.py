7152e7dbd860a6b18c36adbd1cab818c2c2d5ca3
Rohit <rohitjose@gmail.com>
2016-10-06 15:50:55 +1100

    Included logic to lematize the words [score=0.986053922951]

--- title_extraction/word_and_features.py
+++ title_extraction/word_and_features.py
@@ -4,13 +4,13 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction import DictVectorizer
+from nltk.stem.wordnet import WordNetLemmatizer
 
 def check_word_capitalization(word):
     """Checks whether a word is a capitalized word"""
     return_value = False
     if (len(word) > 1):
         return_value = True if (word[0].isupper() and word[1].islower()) else False
-    return return_value
 
 def build_training_features(training_data):
     # Load the data set
@@ -20,31 +20,34 @@ def build_training_features(training_data):
     columns_values = ["word","POStag","TitleMapping","isUpper", "isCapitalized", "isNNP", "isNN", "isJJ","isPrecedingDT","isPrecedingIN","isTitle"]
     preceding_pos_tag = None
 
+    # to lematize a word
+    lmtzr = WordNetLemmatizer()
+
     features_and_labels =[]
     for sentence in training_set:
         for word in sentence:
             token_dict = {}
             # Map the word into the dictionary
-            token_dict[word[0]] = 1
+            token_dict[lmtzr.lemmatize(word[0])] = 1
             # WORD TEXT RELATED FEATURES
             # Check for a word in uppercase
-            token_dict["isUpper"] = (1 if (word[0].isupper()) else 0)
+            token_dict["isUpper"] = (2 if (word[0].isupper()) else 1)
             # Check for word capitalization
-            token_dict["isCapitalized"] = (1 if (check_word_capitalization(word[0])) else 0)
+            token_dict["isCapitalized"] = (2 if (check_word_capitalization(word[0])) else 1)
 
             # WORD POS TAGGING RELATED FEATURES
             # Check if the POS tagging is NNP - (noun, proper, singular)
-            token_dict["isNNP"] = (1 if (word[1] == 'NNP') else 0)
+            token_dict["isNNP"] = (2 if (word[1] == 'NNP') else 1)
             # Check if the POS tagging is NN - (noun, common, singular or mass)
-            token_dict["isNN"] = (1 if (word[1] == 'NN') else 0)
+            token_dict["isNN"] = (2 if (word[1] == 'NN') else 1)
             # Check if the POS tagging is JJ - (adjective or numeral, ordinal)
-            token_dict["isJJ"] = (1 if (word[1] == 'JJ') else 0)
+            token_dict["isJJ"] = (2 if (word[1] == 'JJ') else 1)
 
             # POS TAGGING OF PRECEDING WORD
             # Check if the preceding POS tagging was DT
-            token_dict["isPrecedingDT"] = (1 if (preceding_pos_tag == 'DT') else 0)
+            token_dict["isPrecedingDT"] = (2 if (preceding_pos_tag == 'DT') else 1)
             # Check if the preceding POS tagging was IN (preposition or conjunction, subordinating)
-            token_dict["isPrecedingIN"] = (1 if (preceding_pos_tag == 'IN') else 0)
+            token_dict["isPrecedingIN"] = (2 if (preceding_pos_tag == 'IN') else 1)
 
             # Map the POS tag of the preceding token
             preceding_pos_tag = word[1]
